3695d6a3a6fdab1f1eb014959a96d42ef9ac9494
OlegYch <olegych@tut.by>
2021-11-16 19:24:15 +0200

    try to skip too memory hungry presets

--- src/test/scala/DZIBench.scala
+++ src/test/scala/DZIBench.scala
@@ -2,8 +2,6 @@ import com.google.common.io.{MoreFiles, RecursiveDeleteOption}
 import org.olegych.dzi.imageio.ImageReader
 import org.olegych.dzi.models._
 import org.olegych.dzi.{DZI, ParallelConfig}
-import org.specs2.matcher.ContentMatchers
-import org.specs2.mutable.Specification
 
 import java.io.File
 import java.lang.management.ManagementFactory
@@ -21,16 +19,17 @@ object DZIBench extends App {
   val dzi = DZI(meta.size, tileSize, tileOverlap, tileFormat)
   val bools = List(false, true)
   val ints = List(0, 1024, 10240)
-  val sizes = List(1, 2, 6, 12)
+//  val sizes = List(1, 2, 6, 12)
+  val sizes = List(6, 12)
   val configs = for {
-    cols <- bools
+    cols <- List(false)
     rows = false
     //    rows <- bools
     levels <- bools
     tiles <- bools
     downscale <- ints
-    crop <- ints
-    write <- List(0, 1024)
+    crop <- List(0, 1024)
+    write <- List(0)
     await = 1024
     //    await <- ints
   } yield ParallelConfig(
@@ -54,19 +53,6 @@ object DZIBench extends App {
   } {
     val start = Instant.now()
     val cpuStart = bean.getProcessCpuTime
-    val threads = (0 until size).map { _ =>
-      val outputFolder = new File(rootOutput, Random.nextInt().toString)
-      outputFolder -> new Thread(() => {
-        dzi.create(ColorDepth.Greyscale, outputFolder, "createDZI", debug = false).withPar(input, c)
-      }) {
-        start()
-      }
-    }
-    threads.foreach { case (outputFolder, thread) =>
-      thread.join()
-    }
-    val cpuTotal = (bean.getProcessCpuTime - cpuStart) / 1000 / 1000
-    val totalPerProcess = (cpuTotal.toDouble / size).toInt
     val configString = List(
       "cols" -> c.cols,
       "rows" -> c.rows,
@@ -77,7 +63,37 @@ object DZIBench extends App {
       "write" -> c.write,
       "await" -> c.await,
     ).map { case (k, v) => s"${k}=,${v}," }.mkString
-    println(s"At ${Instant.now} ${configString} for size ,${size}, took ${Instant.now.getEpochSecond - start.getEpochSecond}, cpu total ,$cpuTotal, per process ,$totalPerProcess")
+    @transient var failed = false
+    val threads = (0 until size).map { _ =>
+      val outputFolder = new File(rootOutput, Random.nextInt().toString)
+      outputFolder -> new Thread(() => {
+        try {
+          dzi.create(ColorDepth.Greyscale, outputFolder, "createDZI", debug = false).withPar(input, c)
+        } catch {
+          case e: Throwable =>
+            println(s"${configString} failed with ${e.getMessage}")
+            failed = true
+        }
+      }) {
+        start()
+      }
+    }
+    threads.foreach { case (outputFolder, thread) =>
+      try {
+        if (!failed) thread.join() else {
+          thread.interrupt()
+          thread.join()
+        }
+      } catch {
+        case e: Throwable =>
+          println(e.getMessage)
+      }
+    }
+    val cpuTotal = (bean.getProcessCpuTime - cpuStart) / 1000 / 1000
+    val totalPerProcess = (cpuTotal.toDouble / size).toInt
+    if (!failed) {
+      println(s"At ${Instant.now} ${configString} for size ,${size}, took ,${Instant.now.getEpochSecond - start.getEpochSecond}, cpu total ,$cpuTotal, per process ,$totalPerProcess")
+    }
     threads.foreach { case (outputFolder, thread) =>
       try {
         MoreFiles.deleteRecursively(outputFolder.toPath, RecursiveDeleteOption.ALLOW_INSECURE)
